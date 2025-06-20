b7d42920289e6229cbe84e6ae9105ff5c119ea12
Naman Aggarwal <naaggarwal@microsoft.com>
2025-06-20 23:50:19 +0530

    DSU By Rank & Path Compression

---
+++ DSUByRank.java
@@ -0,0 +1,57 @@
+class DSU {
+    int[] rank;
+    int[] parent;
+
+    public DSU(int n) {
+        rank = new int[n + 1];
+        parent = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int getParent(int u) {
+        if (u == parent[u]) {
+            return u;
+        }
+
+        return parent[u] = getParent(parent[u]);
+    }
+
+    public void makeUnion(int u, int v) {
+        int pu = getParent(u);
+        int pv = getParent(v);
+        if (pu == pv) {
+            return;
+        }
+
+        int rpu = rank[pu];
+        int rpv = rank[pv];
+
+        if (rpu < rpv) {
+            parent[pu] = pv;
+        } else if (rpu > rpv) {
+            parent[pv] = pu;
+        } else {
+            parent[pu] = pv;
+            rank[pv]++;
+        }
+    }
+
+    public boolean isPresentInSameComponent(int u, int v) {
+        return getParent(u) == getParent(v);
+    }
+}
+
+public class DSUByRank {
+    public static void main(String[] args) {
+        DSU dsu = new DSU(7);
+        dsu.makeUnion(1, 2);
+        dsu.makeUnion(2, 3);
+
+        System.out.println(dsu.isPresentInSameComponent(3, 4));
+
+        dsu.makeUnion(1, 4);
+        System.out.println(dsu.isPresentInSameComponent(3, 4));
+    }
+}
